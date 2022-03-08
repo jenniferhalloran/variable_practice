57402fc904a899da0e314ebf094dad1fdc392096
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-03-08 13:15:48 -0500

    Add reassigned variables

--- variables.rb
+++ variables.rb
@@ -1,13 +1,23 @@
+
+# strings
 my_name = "Jennifer"
 cat_name = "Simba"
 favorite_activity = "getting pet"
 
+# integers
 my_age = 28
 cat_age = 13
 pats_a_day = 10000
 
+#floats
 bank_balance = 1.50
 gym_cost = 155.50
 
+#booleans
 is_broke = true
 loves_cats = true
+
+# reassign
+my_name = "Jennifer Lauren Halloran"
+cat_name = "Sir Simba Fluffybutt"
+favorite_activity = "meowing at Jenn when she is busy"
